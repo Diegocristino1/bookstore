979659c11a2725c3f282189920d33a50ad11d693
Diego Cristino Ambrosio <diego.ambrosio@cnpq.lan>
2026-07-15 09:00:06 -0300

    Criando Docker Compose

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_264m@l!!j#bsoaac@)v72+@u_tplzp91ava3j-_7wadm(=@ef"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-_264m@l!!j#bsoaac@)v72+@u_tplzp91ava3j-_7wadm(=@ef")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
@@ -79,8 +80,12 @@ WSGI_APPLICATION = "bookstore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "bookstore"),
+        "USER": os.environ.get("DB_USER", "bookstore"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "bookstore"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
